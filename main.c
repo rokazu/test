c203710800294ed28d3567e0724c4961ebd9bf1e
rokazu <rokazu.master@users.noreply.github.com>
2019-03-29 16:51:40 -0600

    HOLA

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-File *pf=0;
+File *pf=NULL;
 
 int main(int argc, char *argv[]) {
 	return 0;
